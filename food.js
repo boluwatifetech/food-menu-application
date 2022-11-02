e29e67bfb536a2572317ae550a089bca2710afd0
Abiola Boluwatife <boluabiola202@gmail.com>
2022-11-02 08:15:46 -0700

    food menu application

---
+++ food.js
@@ -0,0 +1,270 @@
+const menu = [{
+        id: 1,
+        image: 'images/bangermash.jpeg',
+        dish: 'banger mash',
+        category: 'foreign',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 2,
+        image: 'images/beancake.jpeg',
+        dish: 'bean cake',
+        category: 'local',
+        amount: 24,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 2,
+        image: 'images/puffpuff.jpeg',
+        dish: 'puff puff',
+        category: 'snacks',
+        amount: 24,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 29,
+        image: 'images/fanta.jpeg',
+        dish: 'fanta',
+        category: 'drinks',
+        amount: 24,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 27,
+        image: 'images/redwine.jpeg',
+        dish: 'red wine',
+        category: 'drinks',
+        amount: 24,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 3,
+        image: 'images/chips.jpeg',
+        dish: 'chips',
+        category: 'snacks',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 4,
+        image: 'images/coffee.jpeg',
+        dish: 'coffee',
+        category: 'foreign',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 5,
+        image: 'images/croissant.jpeg',
+        dish: 'croissant',
+        category: 'foreign',
+        amount: 29,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 6,
+        image: 'images/egusi.jpeg',
+        dish: 'egusi',
+        category: 'local',
+        amount: 26,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 7,
+        image: 'images/ewa.jpeg',
+        dish: 'beans',
+        category: 'local',
+        amount: 23,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 8,
+        image: 'images/fried.jpeg',
+        dish: 'freid rice',
+        category: 'foreign',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 9,
+        image: 'images/grilledcheese.jpeg',
+        dish: 'grilled cheese',
+        category: 'foreign',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 10,
+        image: 'images/indomie.jpeg',
+        dish: 'indomie',
+        category: 'foreign',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 11,
+        image: 'images/pancake.jpeg',
+        dish: 'pancake',
+        category: 'foreign',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 12,
+        image: 'images/peppersoup.jpeg',
+        dish: 'pepper soup',
+        category: 'local',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 13,
+        image: 'images/poundedyam.jpeg',
+        dish: 'pounded yam',
+        category: 'local',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 14,
+        image: 'images/salad.jpeg',
+        dish: 'salad',
+        category: 'foreign',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 15,
+        image: 'images/sausage.jpeg',
+        dish: 'sausage',
+        category: 'foreign',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 16,
+        image: 'images/steak.jpeg',
+        dish: 'steak',
+        category: 'foreign',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+
+
+    {
+        id: 18,
+        image: 'images/chinchin.jpeg',
+        dish: 'chinchin',
+        category: 'snacks',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 19,
+        image: 'images/okro.jpeg',
+        dish: 'okro soup',
+        category: 'soup',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 20,
+        image: 'images/pepsi.jpeg',
+        dish: 'pepsi',
+        category: 'drinks',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 21,
+        image: 'images/pie.jpeg',
+        dish: 'meat pie',
+        category: 'snacks',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 22,
+        image: 'images/youghurtimage.jpeg',
+        dish: 'youghurt',
+        category: 'drinks',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 23,
+        image: 'images/semo.jpeg',
+        dish: 'semo',
+        category: 'local',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 24,
+        image: 'images/vegetable.jpeg',
+        dish: 'vegetable soup',
+        category: 'soup',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 25,
+        image: 'images/egusi.jpeg',
+        dish: 'egusi',
+        category: 'soup',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+    {
+        id: 26,
+        image: 'images/bigiapple.jpeg',
+        dish: 'bigi apple',
+        category: 'drinks',
+        amount: 20,
+        word: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa placeat aspernatur magnam quibusdam commodi alias"
+    },
+];
+let container = document.getElementById('full-container');
+const filterBtn = document.querySelectorAll('.filter-btn')
+filterBtn.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const category = e.currentTarget.dataset.class;
+        const menuCategory = menu.filter(function(menuItems) {
+            if (menuItems.category === category) {
+                return menuItems;
+            }
+        })
+        if (category === 'all') {
+            displayFoodItem(menu);
+        } else {
+            displayFoodItem(menuCategory);
+        }
+    })
+})
+
+
+window.addEventListener('DOMContentLoaded', function() {
+    displayFoodItem(menu);
+});
+
+function displayFoodItem(itemsMenu) {
+    let displayMenu = itemsMenu.map(function(item) {
+
+        return `<article class="article">
+    <div class="img-container">
+        <img src=${item.image} class='photo'>
+    </div>
+    <div class="known">
+        <span>
+        <h4 id="food">${item.dish}</h4>
+        <h5 id="price">$${item.amount}</h5>
+       </span>
+        <hr>
+        <p id="text">  ${item.word}</p>
+    </div>
+</article>`;
+    });
+    displayMenu = displayMenu.join('')
+    container.innerHTML = displayMenu;
+}
